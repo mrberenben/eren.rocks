2b2bf6e93227c634628b0554faaf2cbb94bb6f3d
eren <79411240+mrberenben@users.noreply.github.com>
2023-01-07 16:05:37 +0300

    :bento: update: shuffle images

--- src/utils/config/images.ts
+++ src/utils/config/images.ts
@@ -1,9 +1,9 @@
 const Images = {
   shuffle: [
-    require("@/assets/images/shuffle/1.jpg"),
-    require("@/assets/images/shuffle/2.jpg"),
-    require("@/assets/images/shuffle/3.jpg"),
-    require("@/assets/images/shuffle/4.jpg")
+    require("@/assets/images/shuffle/1-min.webp"),
+    require("@/assets/images/shuffle/2-min.webp"),
+    require("@/assets/images/shuffle/3-min.webp"),
+    require("@/assets/images/shuffle/4-min.webp")
   ]
 };
 
